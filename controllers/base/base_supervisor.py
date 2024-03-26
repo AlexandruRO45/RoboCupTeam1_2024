5b7d9311159c6a046eb570067713c2ef93343742
AlexandruRO45 <savaalexandru562@gmail.com>
2024-03-26 03:39:03 +0000

    Update robot names and score display

--- controllers/base/base_supervisor.py
+++ controllers/base/base_supervisor.py
@@ -9,15 +9,15 @@ from controller import Supervisor
 class BaseSupervisor(Supervisor):
     RobotList = [
         # Team Red
-        'robot1', 
-        'robot2', 
-        'robot4',
-        'robot8',
+        'red_goalkeeper', 
+        'red_defender',  
+        'red_sec_attacker',
+        'red_main_attacker',
         # Team Blue
-        'robot5', 
-        'robot3', 
-        'robot6',
-        'robot7',
+        'blue_goalkeeper', 
+        'blue_defender',  
+        'blue_sec_attacker',
+        'blue_main_attacker',
     ]  # List of robot names
 
     def __init__(self):
@@ -39,15 +39,25 @@ class BaseSupervisor(Supervisor):
 
     def updateScoreboard(self):
         # Update the score display on the supervisor
-        score_text = f"Score: {self.score['team1']} - {self.score['team2']}"
+        score_text = f"Score: {self.score['Team Red']} - {self.score['Team Blue']}"
         self.setLabel(0, score_text, 0.4, 0.9, 0.1, 0xffffff, 0, 'Arial')
 
     def isitGoal(self):
         # Check if the ball has entered a goal and update score accordingly
+        for robot in self.robots:
+            if robot.getBoundingBox().intersects(self.ball.getBoundingBox()):
+                if robot.getField('team').getSFString() == 'red':
+                    self.score['Team Blue'] += 1
+                else:
+                    self.score['Team Red'] += 1
+                self.updateScoreboard()
+                self.resetSimulation()
+                break
         # $PLACEHOLDER$
 
     def getBallPosition(self):
         # Get the current 3D coordinates (x, y, z) of the ball
+        
         # $PLACEHOLDER$
 
     def setBallPosition(self, position):
