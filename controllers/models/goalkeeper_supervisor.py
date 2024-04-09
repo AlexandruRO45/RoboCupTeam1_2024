429d5b48800ebffc2fa541e98d75b60f118d4411
AlexandruRO45 <savaalexandru562@gmail.com>
2024-04-09 02:16:49 +0100

    remove unused files

--- controllers/models/goalkeeper_supervisor.py
+++
@@ -1,9 +0,0 @@
-from controller import Supervisor
-from controllers.models.goalkeeper import Goalkeeper
-
-
-class GoalKeeperSupervisor(Goalkeeper, Supervisor):
-
-    def __init__(self):
-        super(GoalKeeperSupervisor, self).__init__()
-        print("GoalKeeperSupervisor created")
